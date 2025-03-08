6d410387875182025caff407d6414be353cbd5d1
Alessandro <alemartiale3@gmail.com>
2025-03-08 21:29:22 +0100

    Added ros_canopen package

---
+++ catkin_ws/src/wheelchair_control/include/wheelchair_control/CANsender.hpp
@@ -0,0 +1,39 @@
+#ifndef CANSENDER_HPP
+#define CANSENDER_HPP
+
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <socketcan_interface/socketcan.h>
+#include <socketcan_interface/filter.h>
+#include <socketcan_interface/interface.h>
+#include <functional>
+
+class CANsender{
+    // Define CAN interface (in my case always can0)
+    private:
+
+        const std::string CAN_INTERFACE = "can0";
+        const uint32_t RNET_JOYSTICK_ID = 0x0CFF0000; //todo: reverse engineer the real one
+        can::SocketCANInterface can_driver;
+        ros::Subscriber joy_sub;
+
+    public:
+
+        uint8_t joystick_x;
+        uint8_t joystick_y;
+
+        CANsender(ros::NodeHandle& nh);
+
+        ~CANsender();
+
+        void injectRnetJoystickFrame();
+
+        void joyCallback(const sensor_msgs::Joy::ConstPtr& msg);
+
+        void canFrameCallback(const can::Frame& frame);
+};
+
+#endif // CANSENDER_HPP
+
+    
+
--- catkin_ws/src/wheelchair_control/src/CANsender.cpp
+++ catkin_ws/src/wheelchair_control/src/CANsender.cpp
@@ -1,89 +1,53 @@
-#include <ros/ros.h>
-#include <sensor_msgs/Joy.h>
-#include <socketcan_interface/SocketCAN.h>
-#include <socketcan_interface/CanFrame.h>
-#include <socketcan_interface/CanReceiver.h>
-#include <socketcan_interface/CanSender.h>
-#include <iomanip>
-#include <sstream>
+#include "../include/wheelchair_control/CANsender.hpp"
 
-// Define CAN interface
-const std::string CAN_INTERFACE = "can0";
+CANsender::CANsender(ros::NodeHandle& nh) {
+    joystick_x = 0x00;
+    joystick_y = 0x00;
+    joy_sub = nh.subscribe("/joy", 10, &CANsender::joyCallback, this);
 
-//CAN ID THIS WILL BE CHANGED WITH REVERSED ENGINEERING
-const uint32_t RNET_JOYSTICK_ID = 0x0CFF0000; /
-
-// Global variables for joystick position
-uint8_t joystick_x = 0x00;
-uint8_t joystick_y = 0x00;
-
-ros::Publisher can_pub;
-socketcan_interface::SocketCAN can_driver;
-socketcan_interface::CanSender::Ptr can_sender;
-
-// Convert integer to 2-digit HEX string
-std::string dec2hex(uint8_t value) {
-    std::stringstream stream;
-    stream << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(value);
-    return stream.str();
+    if (!can_driver.init(CAN_INTERFACE, false)) {
+        ROS_ERROR("Failed to initialize CAN interface!");
+    }
 }
 
-// Function to inject a spoofed joystick frame
-void injectRnetJoystickFrame() {
-    socketcan_interface::CanFrame can_frame;
+CANsender::~CANsender() {}
+// Function to inject a spoofed joystick frame in the wheelchair
+void CANsender::injectRnetJoystickFrame() {
+    // Create a CAN frame
+    can::Frame can_frame;
     can_frame.id = RNET_JOYSTICK_ID;
     can_frame.dlc = 2;  // Only sending joystick X and Y
-
-    // Fill frame with joystick data
+    
+    // Set joystick values
     can_frame.data[0] = joystick_x;
     can_frame.data[1] = joystick_y;
-
-    // Send the frame ASAP
-    can_sender->send(can_frame);
     
-    ROS_INFO("Injected spoofed frame: ID=0x%X Data=[%02X %02X]", can_frame.id, can_frame.data[0], can_frame.data[1]);
+    // Send the frame
+    if (!can_driver.send(can_frame)) {
+        ROS_WARN("Failed to send CAN frame");
+    }
 }
 
 // Callback for joystick messages
-void joyCallback(const sensor_msgs::Joy::ConstPtr& msg) {
-    // Convert joystick X (-1.0 to 1.0) to 0x00 - 0xFF
-    joystick_x = static_cast<uint8_t>((msg->axes[0] + 1.0) * 127); // -1.0 -> 0x00, 1.0 -> 0xFF
+void CANsender::joyCallback(const sensor_msgs::Joy::ConstPtr& msg) {
+    // Map joystick values to 0-255 range
+    joystick_x = static_cast<uint8_t>((msg->axes[0] + 1.0) * 127);
     joystick_y = static_cast<uint8_t>((msg->axes[1] + 1.0) * 127);
-
-    ROS_INFO("Joystick updated: X=%02X, Y=%02X", joystick_x, joystick_y);
-}
-
-// Callback for receiving CAN messages
-void canReceiveCallback(const socketcan_interface::CanFrame &frame) {
-    // Check if the received frame is the expected joystick frame
-    if (frame.id == RNET_JOYSTICK_ID && frame.data[0] == 0x00 && frame.data[1] == 0x00) {
-        ROS_INFO("Received expected joystick frame, injecting spoofed response...");
-        injectRnetJoystickFrame();
-    }
+    
+    // Inject the frame
+    injectRnetJoystickFrame();
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "joy_to_can_spoofer");
+    ros::init(argc, argv, "wheelchair_controller");
     ros::NodeHandle nh;
-
-    // Initialize CAN driver
-    if (!can_driver.init(CAN_INTERFACE, false)) {
-        ROS_ERROR("Failed to initialize CAN interface %s", CAN_INTERFACE.c_str());
-        return -1;
-    }
-
-    // Create CAN sender
-    can_sender = can_driver.createSender();
-
-    // Create CAN receiver and subscribe to joystick CAN ID
-    socketcan_interface::CanReceiver can_receiver;
-    can_receiver.setCallback(canReceiveCallback);
-    can_driver.createReceiver(&can_receiver);
-
-    // Subscribe to joystick topic
-    ros::Subscriber joy_sub = nh.subscribe("joy", 10, joyCallback);
-
-    // Run ROS event loop
+    
+    // Initialize CAN interface
+    CANsender can_sender(nh);
+    
+    ROS_INFO("Wheelchair controller started. Listening for joystick input...");
+    
     ros::spin();
+    
     return 0;
 }
