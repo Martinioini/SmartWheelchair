0ab105aa8f41bf9a2b305e67c6df58f992eb4b4a
Alessandro <alemartiale3@gmail.com>
2025-04-02 09:45:25 +0200

    script fix

--- start_script.c
+++ start_script.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <linux/joystick.h>
 
-#define BUTTON_ID 8
+#define BUTTON_ID 10
 #define START_CMD "roslaunch wheelchair_control real_wheelchair.launch &"
 #define KILL_CMD "pkill -f 'roslaunch wheelchair_control'"
 #define CAN_UP_CMD "sudo ip link set can0 type can bitrate 125000 && sudo ip link set up can0"
@@ -14,18 +14,27 @@ int node_running = 0;  // Flag to track node state
 
 void toggle_ros_node() {
     if (node_running == 0) {
+        // First bring down the CAN interface to ensure clean state
         printf("Bringing down CAN interface...\n");
         system(CAN_DOWN_CMD);
-
+        sleep(1);  // Wait for interface to go down
+        
         printf("Setting up CAN interface...\n");
         system(CAN_UP_CMD);
+        sleep(2);  // Give CAN interface time to initialize
         
         printf("Starting wheelchair control...\n");
         system(START_CMD);
+        sleep(3);  // Give ROS nodes time to start
         node_running = 1;
     } else {
         printf("Stopping wheelchair control...\n");
         system(KILL_CMD);
+        sleep(2);  // Give ROS nodes time to shut down
+        
+        printf("Bringing down CAN interface...\n");
+        system(CAN_DOWN_CMD);
+        sleep(1);  // Wait for interface to go down
         node_running = 0;
     }
 }
@@ -39,10 +48,15 @@ int main() {
 
     printf("Controller started. Press button %d to toggle wheelchair.\n", BUTTON_ID);
     
+    // Set non-blocking mode for joystick
+    fcntl(fd, F_SETFL, O_NONBLOCK);
+    
     struct js_event event;
     while (1) {
-        if (read(fd, &event, sizeof(event)) > 0) {
+        // Read joystick events
+        while (read(fd, &event, sizeof(event)) > 0) {
             if (event.type == JS_EVENT_BUTTON && event.number == BUTTON_ID && event.value == 1) {
+                printf("Button %d pressed - toggling wheelchair control\n", BUTTON_ID);
                 toggle_ros_node();
             }
         }
